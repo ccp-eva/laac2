8748f59b8c19fa1a902fb754986f9e03c651a4ab
Manuel Bohn <manuel0bohn@gmail.com>
2025-09-22 18:21:33 +0200

    add multi lstm script

--- analysis/multi_lstm.R
+++ analysis/multi_lstm.R
@@ -4,6 +4,14 @@ library(brms)
 
 long <- read.csv("../data/laac_complete.txt", sep="", na.strings="-99")
 
+# create categorical variables indicating time point and stable trait level
+long$trial <- as.factor(long$trial)
+long$time_cat <- as.factor(long$time_point)
+long$trait <- 1
+# recode search task to binary response
+long[long$task == "search", "code"] <- car::recode(long[long$task == "search", "code"], '2=1')
+
+
 
 Multi_LST <-  brm(code ~ 0 + time_cat + trial + task + time_cat:task + trial:task
                    + (0 + time_cat + time_cat:task || subject) + (0 + task | subject),
