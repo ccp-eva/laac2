96ac428d36944002dc6538410287ef96401450b5
manuelbohn <manuel_bohn@eva.mpg.de>
2025-09-23 19:23:17 +0200

    add consistency from binomial models

---
+++ analysis/multi_lstm_bin.R
@@ -0,0 +1,35 @@
+
+library(tidyverse)
+library(stringr)
+library(brms)
+
+long <- read.csv("../data/laac_complete.txt", sep="", na.strings="-99")
+
+# create categorical variables indicating time point and stable trait level
+long$trial <- as.factor(long$trial)
+long$time_cat <- as.factor(long$time_point)
+long$trait <- 1
+# recode search task to binary response
+long[long$task == "search", "code"] <- car::recode(long[long$task == "search", "code"], '2=1')
+
+long_bin <- long%>%
+  filter(!is.na(code))%>%
+  group_by(time_point, subject, task, time_cat, trait)%>%
+  summarise(n = length(code), 
+            sum = sum(code))%>%
+  ungroup()
+
+d1 <- long_bin%>%filter(task %in% c("cause", "delay", "gaze", "inference", "quant"))
+
+Multi_LST1_bin <-  brm(sum|trials(n) ~ 0 + time_cat + task + time_cat:task + 
+                  (0 + time_cat + time_cat:task || subject) + (0 + task | subject),
+                  data = d1,
+                  family = binomial(),
+                  chains = 4, # to speed up sampling --> increase later
+                  cores = 4,
+                  backend = "cmdstanr",
+                  threads = threading(10),
+                  control = list(adapt_delta = 0.95, max_treedepth = 20),
+                  iter=10000) # decreased for model testing
+
+saveRDS(Multi_LST1_bin, "../saves/multi_lst1_bin.rds")
